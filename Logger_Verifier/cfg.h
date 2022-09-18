89776827e8cdc8adcb67bf26780780dc6b810a5b
johnkim6823 <82578738+johnkim6823@users.noreply.github.com>
2022-09-18 18:42:36 +0900

    Update cfg.h

--- Logger_Verifier/cfg.h
+++ Logger_Verifier/cfg.h
@@ -1,4 +1,4 @@
-//CHANGE SERVER_IP_ADDR to RPI's IP that working as SERVER
+// CHANGE SERVER_IP_ADDR to RPI's IP that working as SERVER
 # define SERVER_IP_ADDR     "192.168.123.105"
 # define SERVER_PORT        8700
 # define storage_dir        "/home/pi/images/"
@@ -7,7 +7,7 @@
 # define Hash_size          350
 # define CID_size           23
 
-//Default values for Camera Settings
+// Default values for Camera Settings
 #define DEFAULT_WIDTH       640
 #define DEFAULT_HEIGHT      480
 #define DEFAULT_FPS         30
@@ -16,5 +16,10 @@
 #define CMD_HDR_SIZE        8
 #define ASYNC_BUFSIZE       4096
 #define MAX_USER_CNT        5
+
+// CAMERA_CONFIG_TYPE
+#define TYPE_SIZE           1
+#define TYPE_FPS            2
+#define TYPE_FORMAT         3
 
 #define ThisID Server
