c4866ce4d4c238f77da53ab3d3944807f05b9fd1
johnkim6823 <82578738+johnkim6823@users.noreply.github.com>
2022-09-29 17:13:51 +0900

    Update and rename cfg.h to verifier_cfg.h

--- Verifier/verifier_cfg.h
+++ Verifier/verifier_cfg.h
@@ -1,10 +1,5 @@
 #ifndef __VERIFIER_CFG__
 #define __VERIFIER_CFG__
-// CHANGE SERVER_IP_ADDR to RPI's IP that working as SERVER
-# define SERVER_IP_ADDR     "127.0.0.1"
-# define SERVER_PORT        8700
-# define storage_dir        "/home/pi/images/"
-
 // Deafault values for datasize
 # define Hash_size          350
 # define CID_size           23
@@ -12,7 +7,7 @@
 // Default values for Camera Settings
 #define DEFAULT_WIDTH 	   640
 #define DEFAULT_HEIGHT	   480
-#define DEFAULT_FPS	   	   30
+#define DEFAULT_FPS	   	   10
 
 // Communication protocol
 #define CMD_HDR_SIZE        8
