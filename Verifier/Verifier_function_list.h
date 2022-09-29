d31da8a45e6a47b6c108ac5611b615597efbe41e
johnkim6823 <82578738+johnkim6823@users.noreply.github.com>
2022-09-29 17:12:37 +0900

    Update Verifier_function_list.h

--- Verifier/Verifier_function_list.h
+++ Verifier/Verifier_function_list.h
@@ -5,14 +5,14 @@
 
 using namespace std;
 
-void read_pubKey();
+int read_pubKey();
 void init();
 void init_queue();
-void get_data_from_server();								//Get Datas from SERVER
+void get_data_from_server();								          //Get Datas from SERVER
 void convert_frames(queue<cv::Mat> BGR_QUEUE);				//Convert Datas
-void edge_detection(queue<cv::Mat> &Y_QUEUE);               //Edge detact by y frames
-void make_hash(queue<cv::Mat> &FV_QUEUE);                   //make hash using feature vector
-string getCID();                                            //Make CID for each frames
-int coompare_hashs();								        //compare two hashs
-string getCID();                                            //Make CID for frames
+void edge_detection(queue<cv::Mat> &Y_QUEUE);         //Edge detact by y frames
+void make_hash(queue<cv::Mat> &FV_QUEUE);             //make hash using feature vector
+string getCID();                                      //Make CID for each frames
+int coompare_hashs();								                  //compare two hashs
+string getCID();                                      //Make CID for frames
 int make_merkle_tree();
