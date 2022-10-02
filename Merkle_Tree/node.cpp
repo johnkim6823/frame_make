2ea9cd5dc729a511509446cf3baa1cc16850d92b
Myeongjun Kim <johnkim682@gmail.com>
2022-10-03 01:09:47 +0900

    from MJ 1003 v2

--- Merkle_Tree/node.cpp
+++
@@ -1,9 +0,0 @@
-#include "node.h"
-
-Node::Node(){
-    this -> hash = "111";
-}
-
-Node::Node(std::string data) {
-    this->hash = data;
-}
--- TEST/mktree_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../Merkle_Tree/merkle_tree.h"
-
-using namespace std;
-
-int main() {
-    vector<Node*> leaves;
-    cout << "1" << endl;
-}
