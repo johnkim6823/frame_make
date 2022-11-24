79ebf32b83551b76f19cf1767633f01298ace958
johnkim6823 <82578738+johnkim6823@users.noreply.github.com>
2022-11-24 15:14:49 +0900

    Update logger_cfg.h

--- Logger/logger_cfg.h
+++ Logger/logger_cfg.h
@@ -1,14 +1,6 @@
-#ifndef __LOGGER_CFG__
-#define __LOGGER_CFG__
 // Default values for Camera Settings
 #define DEFAULT_WIDTH 			640
 #define DEFAULT_HEIGHT			480
 #define DEFAULT_FPS				30
 #define DEFAULT_FRAME_COUNT 	3600
 //----------------------------------
-#define orifile_path            "/home/pi/Web-UI/static/images/L_original.png"
-#define yfile_path              "/home/pi/Web-UI/static/images/L_Y_frame.png"
-//----------------------------------
-#define ThisID Logger
-
-#endif
